1d8a8810ade42cc758bf47ab96c922bf48c9a334
applesnake <serphiroth@163.com>
2016-10-09 15:28:38 +0800

    src/OCfile.mm include/OCcache.hh include/OCfile.hh mklib/CMakeLists.txt src/OCcachePrivate.mm test/CMakeLists.txt test/test_OCfile.mm

--- src/OCfile.mm
+++ src/OCfile.mm
@@ -2,8 +2,10 @@
 #import "OCcache.hh"
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 @implementation OCfile
+@synthesize f;
 -(id) init{
 	[super init];
 	if([self respondsToSelector:@selector(autoChopLineBreaker:)]) {
@@ -32,7 +34,8 @@
 									 flag:(const char*)flags
 {
 	[super init];
-	f = fopen([filename UTF8String], flags);
+	f = nil;
+	f = fopen(filename, flags);
 	fname = filename;
 	[self autorelease];
 	[self retain];
@@ -42,7 +45,7 @@
 										flag:(OCstring*)flags{
 	[super init];
 	f = fopen([filename UTF8String], [flags UTF8String]);
-	fname = filename;
+	fname = [filename UTF8String];
 	[self autorelease];
 	[self retain];
 	return self;
@@ -52,7 +55,7 @@
 	return ocf;
 };
 +(id) OCfileWithFileCname:(const char*)filename
-										flag:(const char*)flags{
+										 flag:(const char*)flags{
 	OCfile* ocf = [[OCfile alloc] initWithFileCname:filename
 																						 flag:flags];
 	return ocf;
@@ -76,7 +79,7 @@
 	size_t cur = ftell(f);
 	fseek(f, 0, SEEK_END);
 	size_t ed = ftell(f);
-	fseek(f, cur, SEE_SET);
+	fseek(f, cur, SEEK_SET);
 	return ed - cur;
 };
 -(size_t) currentPos{
@@ -89,9 +92,6 @@
 -(void) seekEnd{
 	if(f) fseek(f, 0, SEEK_END);
 };
--(void) truncate:(size_t)off{
-	if(f) ftruncate(f,off);
-};
 -(void) seekAt:(size_t) off{
 	if(f) fseek(f, off, SEEK_SET);
 };
@@ -103,18 +103,21 @@
 -(long) writeBytes:(size_t)n
 				withBuffer:(void*)buf{
 	if(f) {
-		return fwrite(buf, 1, n, buf);
+		return fwrite(buf, 1, n, f);
 	}else {
 		return 0;
 	}
 };
 -(void) close{
-	if(f) fclose(f);
+	if(f) {
+		fclose(f);
+		f = nil;
+	};
 };
 -(OCcache*) readBytes:(size_t)n{
 	if(f) {
 		OCcache* occ = [[OCcache alloc] initWithCharValue:0
-																								count:n];
+																								count:n+1];
 		fread(occ.p, 1, n, f);
 		return occ;
 	}else {
@@ -134,38 +137,35 @@
 	}
 	return 0;
 };
-+(int) writeFileWithCname:(const char*)fname
-						 withProvider:(id)p{
-	FILE* f = fopen(fname, "wb");
-	if(not f) return -1;
-	size_t bytes = [p byteCount];
-	size_t offset = [p writeOffset];
-	fseek(f, offset, SEEK_SET);
-	fwrite([p dataPtr], 1, bytes, f);
-	fclose(f);
-	return 0;
-};
-+(int) writeFileWithOCname:(OCstring*)fname
-							withProvider:(id)p{
-	FILE* f = fopen([fname UTF8String], "wb");
-	if(not f) return -1;
-	size_t bytes = [p byteCount];
-	size_t offset = [p writeOffset];
-	fseek(f, offset, SEEK_SET);
-	fwrite([p dataPtr], 1, bytes, f);
-	fclose(f);
-	return 0;
-};
-+(int) writeFileWithFile:(FILE*)f
-						withProvider:(id)p{
-	if(not f) return -1;
-	size_t bytes = [p byteCount];
-	size_t offset = [p writeOffset];
-	fseek(f, offset, SEEK_SET);
-	fwrite([p dataPtr], 1, bytes, f);
-	return 0;
-};
-
-
-
+// +(int) writeFileWithCname:(const char*)fname
+// 						 withProvider:(id)p{
+// 	FILE* f = fopen(fname, "wb");
+// 	if(not f) return -1;
+// 	size_t bc = [p byteCount];
+// 	size_t offset = [p writeOffset];
+// 	fseek(f, offset, SEEK_SET);
+// 	fwrite([p dataPtr], 1, bytes, f);
+// 	fclose(f);
+// 	return 0;
+// };
+// +(int) writeFileWithOCname:(OCstring*)fname
+// 							withProvider:(id)p{
+// 	FILE* f = fopen([fname UTF8String], "wb");
+// 	if(not f) return -1;
+// 	size_t bytes = [p byteCount];
+// 	size_t offset = [p writeOffset];
+// 	fseek(f, offset, SEEK_SET);
+// 	fwrite([p dataPtr], 1, bytes, f);
+// 	fclose(f);
+// 	return 0;
+// };
+// +(int) writeFileWithFile:(FILE*)f
+// 						withProvider:(id)p{
+// 	if(not f) return -1;
+// 	size_t bytes = [p byteCount];
+// 	size_t offset = [p writeOffset];
+// 	fseek(f, offset, SEEK_SET);
+// 	fwrite([p dataPtr], 1, bytes, f);
+// 	return 0;
+// };
 @end
